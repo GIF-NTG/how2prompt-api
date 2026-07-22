1e91894049473c60461f8e1317ed29d1d3d2b427
Nguyen Khanh Binhe <nguyenkhanhbinh2004@gmail.com>
2026-07-22 09:24:46 +0700

    Initial commit

---
+++ src/main/java/com/example/how2prompt/How2promptApplication.java
@@ -0,0 +1,13 @@
+package com.example.how2prompt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class How2promptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(How2promptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/how2prompt/How2promptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.how2prompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class How2promptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
