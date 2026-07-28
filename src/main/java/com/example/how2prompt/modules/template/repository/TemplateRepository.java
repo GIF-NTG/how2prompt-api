cac7e2f3bacc45e120198ae3af09b31000db38c1
Nguyen Khanh Binhe <nguyenkhanhbinh2004@gmail.com>
2026-07-28 10:05:12 +0700

    import JpaSpecificationExecutor

--- src/main/java/com/example/how2prompt/modules/template/repository/TemplateRepository.java
+++ src/main/java/com/example/how2prompt/modules/template/repository/TemplateRepository.java
@@ -2,6 +2,7 @@ package com.example.how2prompt.modules.template.repository;
 
 import com.example.how2prompt.modules.template.entity.Template;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
